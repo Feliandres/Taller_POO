1a0f08ebc71936d75c3d2134a2d42514c2ae66be
Feliandres <85601974+Feliandres@users.noreply.github.com>
2022-07-05 11:45:49 -0500

    Changes by ESFOT-22-A

--- src/EnteroLong.java
+++ src/EnteroLong.java
@@ -1,4 +1,25 @@
 public class EnteroLong {
     private int var_entero;
-    pri
+    private long var_long;
+
+    public EnteroLong(int var_entero, long var_long) {
+        this.var_entero = var_entero;
+        this.var_long = var_long;
+    }
+
+    public int getVar_entero() {
+        return var_entero;
+    }
+
+    public void setVar_entero(int var_entero) {
+        this.var_entero = var_entero;
+    }
+
+    public long getVar_long() {
+        return var_long;
+    }
+
+    public void setVar_long(long var_long) {
+        this.var_long = var_long;
+    }
 }
